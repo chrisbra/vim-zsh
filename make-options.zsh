30aec5f3cc21e73df0fa4e55481103d8aec7ad12
Christian Brabandt <cb@256bit.org>
2025-02-09 19:32:29 +0100

    make-options.zsh: inject extra nowarnnestedvar
    Inject an extra nowarnnestedvar variable, so that it can be generated as
    syntax keyword.
    
    While at it, make sure the generated output does not contain trailing
    white spaces, so drop them using a sed command.
    
    related: #57
    
    Signed-off-by: Christian Brabandt <cb@256bit.org>

--- make-options.zsh
+++ make-options.zsh
@@ -4,10 +4,15 @@
 src=$1
 
 typeset -U all=()
-for opt in $(grep '^pindex([A-Za-z_]*)$' "$src/Doc/Zsh/options.yo"); do
+
+# inject NOWARNNESTEDVAR so that a syntax keyword nowarnnestedvar is generated
+# see #57
+for opt in $(sed -ne '/^pindex(NO_WARNNESTEDVAR)$/a pindex(NOWARNNESTEDVAR)' \
+                  -e '/^pindex([A-Za_z_]*)$/p' < "$src/Doc/Zsh/options.yo"); do
 	all+=(${${(L)opt#pindex\(}%\)})
 done
 
 IFS=$'\n' lines=($(fold -sw100 <<<${(oj: :)all}))
 print -r 'syn keyword zshOption nextgroup=zshOption,zshComment skipwhite contained'
-print    "           \\\ ${(j:\n           \\ :)lines}"
+print    "           \\\ ${(j:\n           \\ :)lines}" | \
+  sed 's/ \+$//g' # drop trailing whitespace
